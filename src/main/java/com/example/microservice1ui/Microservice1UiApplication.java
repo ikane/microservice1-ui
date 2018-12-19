9433bd7c23b444c421bcf6fdd76f9bc7a3b42d44
ikane <ibrahima.kane@verifone.com>
2018-12-19 16:29:55 +0100

    First commit

---
+++ src/main/java/com/example/microservice1ui/Microservice1UiApplication.java
@@ -0,0 +1,12 @@
+package com.example.microservice1ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservice1UiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservice1UiApplication.class, args);
+	}
+}
